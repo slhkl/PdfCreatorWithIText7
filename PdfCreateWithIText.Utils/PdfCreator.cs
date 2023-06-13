9f5dd462223cd91d6697171843479c8513ed3c0b
Salih Kol <salih.kol@detaysoft.com>
2023-06-13 11:31:26 +0300

    added turkish chracter support.

--- PdfCreateWithIText.Utils/PdfCreator.cs
+++ PdfCreateWithIText.Utils/PdfCreator.cs
@@ -1,4 +1,6 @@
-﻿using iText.Kernel.Pdf;
+﻿using iText.IO.Font.Constants;
+using iText.Kernel.Font;
+using iText.Kernel.Pdf;
 using iText.Layout;
 using iText.Layout.Element;
 using iText.Layout.Properties;
@@ -13,13 +15,20 @@ namespace PdfCreateWithIText.Utils
             {
                 PdfDocument pdfDocument = new PdfDocument(writer);
                 Document document = new Document(pdfDocument);
-                Paragraph header = new Paragraph("Header")
-                    .SetTextAlignment(TextAlignment.CENTER)
-                    .SetFontSize(20);
 
-                document.Add(header);
+                PdfFont font = PdfFontFactory.CreateFont(StandardFonts.HELVETICA, "windows-1254");
+                Paragraph paragraph = new Paragraph("ÜĞŞİÖÇ\nUGSIOC")
+                    .SetTextAlignment(TextAlignment.LEFT)
+                    .SetMarginTop(115)
+                    .SetMarginLeft(35)
+                    .SetFontSize(10)
+                    .SetWidth(200)
+                    .SetFont(font);
+
+                document.Add(paragraph);
                 document.Close();
             }
+
         }
     }
 }
